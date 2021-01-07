6a41bc876bd04f0dfa24971eac1cb04e3f18e56a
qmya <qandeelgul90@gmail.com>
2021-01-07 10:22:04 -0500

    Now writing the above textOut string to another file

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const fs = require("fs"); //fs Stands for file system, 👉🏽 will give access
 const textIn = fs.readFileSync("./txt/input.txt", "utf-8"); //synchrornous version of file reading
 
 console.log(textIn);
+
+//Writing data into the file
+
+const textOut = `This is what awe know about the avacado: ${textIn}.\nCreated on ${Date.now()}`;
+
+//Now writing the above textOut string to another file
+
+fs.writeFileSync("./txt/output.txt", textOut);
+console.log("File has been written");
